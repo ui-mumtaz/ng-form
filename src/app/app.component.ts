0f5705c5457cf8eee16c379e1a97a9bf7793e658
mumtaz <mumtazahmadui@gmail.com>
2019-09-15 13:51:56 +0530

    simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ang-form';
+export class AppComponent implements OnInit {
+  title = 'Ang Form';
+  signupForm: FormGroup;
+  firstName:string;
+  lastName:string;
+  email:string;
+  password: string;
+
+  constructor(private formBuilder: FormBuilder){
+    this.signupForm = formBuilder.group({
+      fname: new FormControl(),
+      lname:new FormControl(),
+      email: new FormControl(),
+      pwd: new FormControl()
+    })
+  }
+  ngOnInit(){
+
+  }
+
+  postData(signupForm: NgForm){
+    console.log(signupForm.controls);
+  }
 }
