3328be059c0e89bbfa32afec55e69b9539380ccd
mumtaz <mumtazahmadui@gmail.com>
2019-09-15 20:17:47 +0530

    add reset functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,21 +16,35 @@ export class AppComponent implements OnInit {
 
   constructor(private formBuilder:FormBuilder){
     this.signupForm=formBuilder.group({
-      fname: new FormControl(),
-      lname:new FormControl(),
-      email: new FormControl(),
-      pwd: new FormControl()
+      fname: ['', Validators.required],
+      lname: ['', [Validators.required, Validators.maxLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      pwd:   ['', Validators.required]
     })
   }
   ngOnInit(){
 
   }
 
-  postData(signupForm:any){
+ /* postData(signupForm:any){
     this.firstName = signupForm.controls.fname.value;
     this.lastName = signupForm.controls.lname.value;
     this.email = signupForm.controls.email.value;
     this.password = signupForm.controls.pwd.value;
     console.log(this.firstName);
+  } */
+  postData(){
+    this.firstName=this.signupForm.get('fname').value;
+    this.lastName = this.signupForm.get('lname').value;
+    console.log(this.signupForm.value)
+  }
+ /* resetForm(){
+    this.signupForm.reset();
+  } */
+  resetForm(){
+    this.signupForm.reset({
+      fname: "Ahmad",
+      email:'xyz@gmail.com'
+    })
   }
 }
